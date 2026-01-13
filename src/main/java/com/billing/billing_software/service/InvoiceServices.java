212d617053d6a57d05545d9ffa83ca6f01476456
Vishranti Meshram <vishrantimeshram11@gmail.com>
2026-01-13 11:21:06 +0530

    version of Billing Software

--- src/main/java/com/billing/billing_software/service/InvoiceServices.java
+++ src/main/java/com/billing/billing_software/service/InvoiceServices.java
@@ -1,9 +1,4 @@
-//package com.billing.billing_software.service;
-//
-//import com.billing.billing_software.DTOs.InvoiceRequestDTO;
-//import com.billing.billing_software.model.Invoice;
-//
-//public interface InvoiceServices {
-//
-//    Invoice generateInvoice(InvoiceRequestDTO request);
-//}
+package com.billing.billing_software.service;
+
+public class InvoiceServices {
+}
